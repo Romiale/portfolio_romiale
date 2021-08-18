88c664677eda94bb944505c203fa070f500457ed
Romiale <romialengoy5@gamil.com>
2021-08-18 14:01:29 +0200

    feature/modify footer section structure and add style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,25 +9,28 @@ import '../style/footer.scss';
 function Footer() {
     return (
         <footer>
-            <div className="footer_information">
-                <img src={logo} alt="logo portfolio romiale" />
-                <p className="title"> Bésoin de plus d'informations ?</p>
-                <p>Rétrouvez-moi sur les réseaux sociaux, je répond plus rapidement aux messages et mails. à bientot !</p>
-                <div className="reseaux">
-                    <img src={facebook} alt="facebook" />
-                    <img src={email} alt="email" />
-                    <img src={linkedin} alt="linkedin" />
+            <div className="footer_group_info">
+                <div className="footer_information">
+                    <img src={logo} alt="logo portfolio romiale" />
+                    <p className="title"> Bésoin de plus d'informations ?</p>
+                    <p>Rétrouvez-moi sur les réseaux sociaux, je répond plus rapidement aux messages et mails. à bientot !</p>
+                    <div className="reseaux">
+                        <img src={facebook} alt="facebook" />
+                        <img src={email} alt="email" />
+                        <img src={linkedin} alt="linkedin" />
+                    </div>
+                </div>
+                <div>
+                    <address>
+                        <p>N° 116 Avenue Kapanga</p>
+                        <p>Commune de Kinshasa - Kinshasa</p>
+                        <p>République Démocratique du Congo</p>
+                        <a href="mailto:romialengoy5@gmail.com">romialengoy5@gmail.com</a> <br />
+                        <a href="tel:+243822332932">+243822332932</a>
+                    </address>
                 </div>
             </div>
-            <div>
-                <address>
-                    <p>N° 116 Avenue Kapanga</p>
-                    <p>Commune de Kinshasa - Kinshasa</p>
-                    <p>République Démocratique du Congo</p>
-                    <a href="mailto:romialengoy5@gmail.com">romialengoy5@gmail.com</a> <br />
-                    <a href="tel:+243822332932">+243822332932</a>
-                </address>
-            </div>
+            <p>&#169;  Copyright  2020 Tous droits réservés</p>
         </footer>
     )
 }
