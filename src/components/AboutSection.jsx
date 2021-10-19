1e5f0c76d9698475a2b024e071d7845f9e020c88
Romiale <romialengoy5@gmail.com>
2021-10-19 11:30:01 +0200

    style: add cursor  annimation on about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -13,7 +13,7 @@ function AboutSection() {
                     <h2 className="container" id="apropos">À propos de moi</h2>
                     <hr />
                     <p>Je suis développeur fullstack pationné du numérique avec beaucoup d'expérience dans le monde du développement web et web mobile, avec plusieurs réalisations validées par des clients satisfaits.</p>
-                    <p> Pour moi la qualité du code, la satisfaction du client, une équipe motivée sont primordiales.</p>
+                    <p> Pour moi la qualité du code, la satisfaction du client, une équipe motivée sont primordiales<span>|</span> </p>
                     <a  className="btn btn-primary" href="mailto:romialengoy5@gmail.com">Me contacter</a>
                     <a className="btn btn-outline-primary" href={cv} >Télécharger mon CV</a>
                 </div>
